ff212dd79baa922eedad03740f838df2f1b1c930
Robert Collins <rcollins@redmug.co.uk>
2022-05-08 12:09:49 +0100

    Fixed issue when no tests specified

--- src/testsGeneration.js
+++ src/testsGeneration.js
@@ -4,7 +4,7 @@ export const doGenerateTests = async (step, thisRoute, targetRootDir) => {
   let route = thisRoute.name.toLowerCase()
   let arr = thisRoute.testmatches.substring(0, thisRoute.testmatches.indexOf('['))
   let tests = thisRoute.testmatches.split('|')
-  // console.log('tests', tests)
+  console.log('tests', tests, 'tests[0]', tests[0])
   let sendBody = ''
   let statusCodeMatch = 'expect(response.statusCode).toBe(200)'
   if (thisRoute.method === 'POST' || thisRoute.method === 'PUT' || thisRoute.method === 'DELETE') {
@@ -14,24 +14,25 @@ export const doGenerateTests = async (step, thisRoute, targetRootDir) => {
     }
   }
   let matchStr = ''
+  if (tests[0] !== '') {
+    for (let i = 0; i < tests.length; i++) {
+      let arrIndex = -1
+      arrIndex = parseInt(tests[i].substring(tests[i].indexOf('[') + 1, tests[i].indexOf(']')))
+      // console.log('arrIndex:', arrIndex)
+      let objKey = tests[i].substring(tests[i].lastIndexOf('.') + 1)
+      if (thisRoute.requestresponse.data !== '') {
+        let obj = thisRoute.requestresponse.data[arr]
+        let innerObj = obj[arrIndex]
 
-  for (let i = 0; i < tests.length; i++) {
-    let arrIndex = -1
-    arrIndex = parseInt(tests[i].substring(tests[i].indexOf('[') + 1, tests[i].indexOf(']')))
-    // console.log('arrIndex:', arrIndex)
-    let objKey = tests[i].substring(tests[i].lastIndexOf('.') + 1)
-    if (thisRoute.requestresponse.data !== '') {
-      let obj = thisRoute.requestresponse.data[arr]
-      let innerObj = obj[arrIndex]
+        let match = innerObj[objKey]
+        // console.log('innerObj: ', innerObj, 'match: ', match, 'typeOf match:', typeof match)
 
-      let match = innerObj[objKey]
-      // console.log('innerObj: ', innerObj, 'match: ', match, 'typeOf match:', typeof match)
-
-      if (typeof match === 'number' || typeof match === 'boolean') {
-        matchStr += `expect(response.body.data.${arr}[${arrIndex}].${objKey}).toEqual(${match})\n`
-      } else {
-        // Assumes string and assertion quoted
-        matchStr += `expect(response.body.data.${arr}[${arrIndex}].${objKey}).toEqual('${match}')\n`
+        if (typeof match === 'number' || typeof match === 'boolean') {
+          matchStr += `expect(response.body.data.${arr}[${arrIndex}].${objKey}).toEqual(${match})\n`
+        } else {
+          // Assumes string and assertion quoted
+          matchStr += `expect(response.body.data.${arr}[${arrIndex}].${objKey}).toEqual('${match}')\n`
+        }
       }
     }
   }
